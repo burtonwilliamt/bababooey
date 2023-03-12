211cfbbc814234fa006717946e7c7fe813fe44f4
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2023-03-12 18:07:13 -0400

    Move main.py into racket.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 
-import discord
-from racket import setup_logging, RacketBot
+import racket
 
 from bababooey import setup_db
 from bababooey.cogs import BababooeyCog
@@ -11,23 +10,9 @@ _log = logging.getLogger(__name__)
 
 
 def main():
-    setup_logging()
     setup_db()
 
-    intents = discord.Intents.default()
-    intents.typing = False  # pylint: disable=assigning-non-slot
-    intents.presences = False  # pylint: disable=assigning-non-slot
-    intents.message_content = False  # pylint: disable=assigning-non-slot
-
-    _log.info('Creating bot object.')
-    bot = RacketBot(intents=intents, guild_ids=GUILD_IDS)
-
-    _log.info('Adding sound effect commands.')
-    bot.add_cog(BababooeyCog(bot))
-
-    _log.info('Starting the bot.')
-    # Set the log_handler to avoid double logging settup.
-    bot.run(BOT_TOKEN, log_handler=None)
+    racket.run_cog(BababooeyCog, guilds=GUILD_IDS, token=BOT_TOKEN)
 
 
 if __name__ == '__main__':
